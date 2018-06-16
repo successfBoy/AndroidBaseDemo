3aaac167248b55083b440a70d99ec24994dd64cb
李鹏成 <lipengcheng1@ZBMAC-C02VP4ZTH.local>
2018-06-16 18:30:44 +0800

    init project
    Signed-off-by: 李鹏成 <lipengcheng1@ZBMAC-C02VP4ZTH.local>

---
+++ app/src/main/java/com/lpc/androidbasedemo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lpc.androidbasedemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lpc.androidbasedemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
